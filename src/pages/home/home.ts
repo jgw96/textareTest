c4d6ad33258a8984845d8e5275aa8aa9b6009398
jgw96 <jgw9617@gmail.com>
2017-03-30 14:46:17 -0500

    add method

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,12 +11,18 @@ import { ModalPage } from '../modalPage/modalPage';
 export class HomePage {
 
   constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
-
+    this.doWork();
   }
 
   openModal() {
     const modal = this.modalCtrl.create(ModalPage);
     modal.present();
+  }
+
+  doWork() {
+    for (let i = 0; i < 1000; i++) {
+      console.log (i + 1);
+    }
   }
 
 }
